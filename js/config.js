900a1738e8a2fcd987067d9baaba5309b6eaf76d
Moonishe <rekoslxrdoffical@mail.ru>
2026-05-08 19:04:32 +0700

    Update Supabase config to new NBC project

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-window.SUPABASE_URL = 'https://hbzdbigmfawgcvzeljdh.supabase.co';
-window.SUPABASE_ANON_KEY = 'sb_publishable_F_TudEEfDy-KiO-9RTsMYg_9O2WTNYo';
+window.SUPABASE_URL = 'https://rwtogupudmlgkqtbhgup.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_ZcBYoLL06DIDQVyKWctV1w_JSbSoqNf';
 window.TELEGRAM_BOT_USERNAME = 'neurobenchbot';
