5b31abbed79fa803658a23398f0768089e0f6b98
Moonishe <rekoslxrdoffical@mail.ru>
2026-05-08 19:11:38 +0700

    Update Telegram bot username to neurobench_bot

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 window.SUPABASE_URL = 'https://rwtogupudmlgkqtbhgup.supabase.co';
 window.SUPABASE_ANON_KEY = 'sb_publishable_ZcBYoLL06DIDQVyKWctV1w_JSbSoqNf';
-window.TELEGRAM_BOT_USERNAME = 'neurobenchbot';
+window.TELEGRAM_BOT_USERNAME = 'neurobench_bot';
