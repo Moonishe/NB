f1932b84c1a6fe1bf3a7484651ffa069f9a8d6d1
Moonishe <rekoslxrdoffical@mail.ru>
2026-05-09 23:42:21 +0700

    feat: update Turnstile site key to new Cloudflare widget

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_URL = 'https://rwtogupudmlgkqtbhgup.supabase.co';
 window.SUPABASE_ANON_KEY = 'sb_publishable_ZcBYoLL06DIDQVyKWctV1w_JSbSoqNf';
 window.TELEGRAM_BOT_USERNAME = 'neurobench_bot';
-window.TURNSTILE_SITE_KEY = '0x4AAAAAABDkPzF2HfJzXoQy';
+window.TURNSTILE_SITE_KEY = '0x4AAAAAADMH5PrkPcPP-0tA';
